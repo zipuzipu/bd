09c7d354431006ab9504bcbd2f6379a8fc4ed704
zipuzipu <99523392+zipuzipu@users.noreply.github.com>
2022-04-21 02:25:00 +0600

    Add files via upload

--- gbook.inc.php
+++ gbook.inc.php
@@ -18,19 +18,18 @@
 	$name = $_POST['name'];
 	$email = $_POST['email'];
 	$msg = $_POST['msg'];
-	$mysql->query("INSERT INTO msgs (name, email, msg) VALUES ('$name','$email', '$msg')");
 
+	if (($name != "") and ($email != "") and ($msg != "")){
+	$mysql->query("INSERT INTO msgs (name, email, msg) VALUES ('$name','$email', '$msg')");
+	}
 
 
 /* Сохранение записи в БД */
 
-/* Удаление записи из БД */
-
-/* Удаление записи из БД */
 ?>
 <h3>Оставьте запись в нашей Гостевой книге</h3>
 
-<form method="POST" action="<?= $_SERVER['REQUEST_URI']?>">
+<form method="POST" action="<?= $_SERVER['REQUEST_URI']?>" >
 Имя: <br /><input type="text" name="name" /><br />
 Email: <br /><input type="text" name="email" /><br />
 Сообщение: <br /><textarea name="msg"></textarea><br />
@@ -42,17 +41,21 @@ Email: <br /><input type="text" name="email" /><br />
 </form>
 <?php
 /* Вывод записей из БД */
-$select_query = "SELECT name, email, msg FROM msgs ORDER BY id DESC";
+$select_query = "SELECT id, name, email, msg FROM msgs ORDER BY id DESC";
 $result = $mysql->query($select_query);
 $row = mysqli_fetch_array($result);
 $count = 0;
-do{
+date_default_timezone_set('Asia/Almaty');
+?>
+<?php
+while($row = mysqli_fetch_array($result)){
 	echo "<p><table border='0'>
 	<tr>
-	<td><strong>".$row['name']."</strong> &nbsp &nbsp".$row['email']."</td>
+	<td><strong>".$row['name']."</strong> &nbsp &nbsp".date('d-m-y  h:i:s')."&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp
+	<a style = 'color:black' href='index.php?id=gbook&del=".$row['id']."'>Удалить</a></td>
 	</tr>
 	<tr>
-	<td></td>
+	<td>".$row['email']. "<input type='hidden' name='id' value='" . $row["id"] . "' /> </td>
 	<td></td>
 	</tr>
 	<tr>
@@ -63,7 +66,11 @@ do{
 	$count++;
 
 }
-while($row = mysqli_fetch_array($result));
+
 echo "<p>Всего записей в гостевой книге:".$count." </p>";
 /* Вывод записей из БД */
+/* Удаление записи из БД */
+$mysql->query("DELETE * FROM msgs WHERE id = $del");
+
+/* Удаление записи из БД */
 ?>
